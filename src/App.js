9a31cdf64791533ccd4eafeb9351fc6fdcb724f2
Soniya Kambli <kamblisoniya@gmail.com>
2023-10-22 01:34:58 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	new file:   src/ContactForm.js 	modified:   src/index.css 	new file:   tailwind.config.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ContactForm from './ContactForm';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <ContactForm />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/ContactForm.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+const ContactForm = () => {
+  return (
+    <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded shadow-md">
+      <h2 className="text-2xl font-semibold mb-6">Contact Us</h2>
+      <form>
+        <div className="mb-4">
+          <label htmlFor="name" className="block text-sm font-medium text-gray-600">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="mt-1 p-2 w-full border rounded-md"
+            placeholder="Your Name"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-sm font-medium text-gray-600">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="mt-1 p-2 w-full border rounded-md"
+            placeholder="Your Email"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="message" className="block text-sm font-medium text-gray-600">
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            rows="4"
+            className="mt-1 p-2 w-full border rounded-md"
+            placeholder="Your Message"
+            required
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
